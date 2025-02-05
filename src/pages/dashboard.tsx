7b2986a075d764dc4867b9a43e49261632f7424b
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-02-05 11:08:01 -0300

    improvement: managing query stages

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,24 +1,34 @@
 import { getContracts } from '@actions'
-import { useQuery } from '@tanstack/react-query'
+import { QueryStatus, useQuery } from '@tanstack/react-query'
 import { ContractT } from '@type/contract'
 
+const Content = (
+  props: React.HTMLAttributes<HTMLElement> & {
+    status: QueryStatus
+  },
+) => {
+  switch (props.status) {
+    case 'success':
+      return props.children
+    case 'error':
+      return <span>Error!</span>
+    case 'pending':
+    default:
+      return <span>Loading...</span>
+  }
+}
+
 export const Dashboard = () => {
-  const { data, isLoading } = useQuery<ContractT[]>({
+  const { data, status } = useQuery<ContractT[]>({
     queryKey: ['get-contracts'],
     queryFn: getContracts,
   })
 
   return (
     <div className='flex h-screen w-screen flex-col items-center justify-center bg-blue-900 text-white'>
-      {isLoading ? (
-        <span>Loading...</span>
-      ) : (
-        <>
-          {data?.map(contract => (
-            <span key={contract.id}>{contract.name}</span>
-          ))}
-        </>
-      )}
+      <Content status={status}>
+        {data?.map(contract => <span key={contract.id}>{contract.name}</span>)}
+      </Content>
     </div>
   )
 }
