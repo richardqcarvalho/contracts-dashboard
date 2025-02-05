05fc1961bb4c94c25c43450735ba61d29bb726fc
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-02-05 14:57:58 -0300

    improvements: cleaning code avoiding to repeat buttons

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,33 +3,24 @@ import { useNavigate } from 'react-router'
 
 export const Sidebar = () => {
   const navigate = useNavigate()
+  const redirects = [
+    { path: '/dashboard', label: 'Home' },
+    { path: '/dashboard/charts', label: 'Charts' },
+    { path: '/dashboard/metrics', label: 'Metrics' },
+    { path: '/dashboard/contracts', label: 'Contracts' },
+  ]
 
   return (
     <aside className='flex h-full w-48 flex-col items-center justify-center gap-4 bg-blue-800 p-4'>
-      <Button
-        className='w-full'
-        onClick={() => navigate('/dashboard')}
-      >
-        Home
-      </Button>
-      <Button
-        className='w-full'
-        onClick={() => navigate('/dashboard/metrics')}
-      >
-        Metrics
-      </Button>
-      <Button
-        className='w-full'
-        onClick={() => navigate('/dashboard/charts')}
-      >
-        Charts
-      </Button>
-      <Button
-        className='w-full'
-        onClick={() => navigate('/dashboard/contracts')}
-      >
-        Contracts
-      </Button>
+      {redirects.map(button => (
+        <Button
+          key={button.path}
+          className='w-full'
+          onClick={() => navigate(button.path)}
+        >
+          {button.label}
+        </Button>
+      ))}
     </aside>
   )
 }
