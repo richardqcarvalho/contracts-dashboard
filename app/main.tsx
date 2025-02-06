70f9a119de7d4932592ae061534153ade20a7591
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-02-06 13:35:37 -0300

    chore: parsing prettier

--- app/main.tsx
+++ app/main.tsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
 
 const queryClient = new QueryClient()
-
 const root = document.getElementById('root') as HTMLElement
 
 createRoot(root).render(
